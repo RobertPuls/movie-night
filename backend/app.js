d9b3e8b165b9258df5893c974f6bd36a2144c69a
Robert Puls <rpu1s@sbcglobal.net>
2017-09-02 19:10:48 -0600

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 const cors = require('cors');
-const routes = require('./routes');
+const routes = require("./routes");
 
 var app = express();
 
@@ -12,15 +12,17 @@ app.use(bodyParser.urlencoded({
 
 app.use(cors());
 
-app.use('/movies', routes);
+app.use("/movies", routes);
 
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-app.use(function(req, res, next) {
+// error handler
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
@@ -29,7 +31,6 @@ app.use(function(req, res, next) {
 });
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
   console.log("ready");
 });
